ad31f0f5acf874f995c1d8af1b4196531507b883
Tudor <rodut13@gmail.com>
2019-04-12 10:48:00 +0300

    Tudor este zeu coin felguard e viata!

--- hello.c
+++ hello.c
@@ -1,6 +1,4 @@
 #include<stdio.h>
 int main(int argc,char *argv[])
-{
-if(argc!=2){printf("USAGE: %s name\n",argv[0]); return -1;}
-printf("Hello,%s!\n",argv[1]);
+{printf("Salut Tudor!");
 return 0;}
