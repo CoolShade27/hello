3e3975c3ee757bbbc92ad495c4731e09a69d8367
andrei <andrei.calin321@yahoo.com>
2019-04-12 10:35:35 +0300

    altceva

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-int main()
-{ printf("hail,friend!");
+int main(int argc,char *argv[])
+{
+if(argc!=2){printf("USAGE: %s name\n",argv[0]); return -1;}
+printf("Hello,%s!\n",argv[1]);
 return 0;}
